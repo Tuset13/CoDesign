cdc36b52f9408bd6b8d366abc26d33e2872208c5
Pablo <serranopablonavarro@gmail.com>
2020-04-11 19:10:53 +0200

    Login Page Funcionality

--- app/src/main/java/com/example/codesign/LoginActivity.java
+++ app/src/main/java/com/example/codesign/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.codesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button buttonLogin = findViewById(R.id.Login);
+
+        buttonLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
