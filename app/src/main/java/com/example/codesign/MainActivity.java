0936a6118c43ec13cc58015de2bb7e264d3febfe
Tuset13 <37141132+Tuset13@users.noreply.github.com>
2020-05-17 17:59:51 +0200

    Login with Firebase.

--- app/src/main/java/com/example/codesign/MainActivity.java
+++ app/src/main/java/com/example/codesign/MainActivity.java
@@ -1,20 +1,40 @@
 package com.example.codesign;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.codesign.Projecte.ProjectActivity;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements ProjectListFragment.ProjecteListener, View.OnClickListener {
 
+    private static final int RC_SIGN_IN = 5;
+
+    ConnectivityManager cm;
+    NetworkInfo activeNetwork;
     Button npButton;
     Button iButton;
 
@@ -23,11 +43,23 @@ public class MainActivity extends AppCompatActivity implements ProjectListFragme
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Autenticació a través de FirebaseUI
+        firebaseUIAuth();
+
         npButton = findViewById(R.id.NouProjecte);
         iButton = findViewById(R.id.Invitacions);
 
         npButton.setOnClickListener(this);
         iButton.setOnClickListener(this);
+
+        cm =(ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if(cm != null)
+        {
+            activeNetwork = cm.getActiveNetworkInfo();
+
+            new MainActivity.CheckConnectivityTask().execute();
+        }
 
         //INSTANCIEM EL FRAGMENT AMB LA LLISTA DE PROJECTES
         ProjectListFragment frgList = (ProjectListFragment) getSupportFragmentManager()
@@ -55,7 +87,14 @@ public class MainActivity extends AppCompatActivity implements ProjectListFragme
                 return true;
 
             case R.id.exit:
-                finish();
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                finish();
+                            }
+                        });
                 return true;
 
             default:
@@ -86,5 +125,62 @@ public class MainActivity extends AppCompatActivity implements ProjectListFragme
         Intent i = new Intent(this, ProjectActivity.class);
         i.putExtra(getString(R.string.id_key), id);
         startActivity(i);
+    }
+
+    private class CheckConnectivityTask extends AsyncTask<Activity, Void, String> {
+
+        @Override
+        protected String doInBackground(Activity... activity) {
+
+            if(activeNetwork != null && activeNetwork.isConnectedOrConnecting())
+            {
+                if(activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                {
+                    return (String) getText(R.string.wifiCon);
+                }
+                else if(activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+                {
+                    return (String) getText(R.string.mobCon);
+                }
+            }
+            return (String) getText(R.string.noCon);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast toast = Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
+    private void firebaseUIAuth(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setLogo(R.drawable.logo)
+                        .build(), RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if(resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(MainActivity.this, "Authentication success", Toast.LENGTH_LONG).show();
+
+            }else{
+                Toast.makeText(MainActivity.this, "Authentication failed", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
     }
 }
